1f1570f581531267a3dfa3c19d6663cec2bdf9bb
Abhijeet Mohan <void.aby@gmail.com>
2016-10-08 23:39:19 +0530

    Add request timeout script

---
+++ timeout.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	ch          chan bool
+	timeoutSecs uint = 3
+	workSecs    uint = 2
+)
+
+func main() {
+	timeoutChan := make(chan bool, 1)
+	ch = make(chan bool, 1)
+	go SlowProcessingWork(timeoutChan)
+
+	select {
+	case <-ch:
+		fmt.Println("The function worked")
+	case <-timeoutChan:
+		fmt.Println("Timeout!")
+	}
+}
+
+func SlowProcessingWork(timeoutChan chan bool) {
+	go timeout(timeoutChan)
+
+	time.Sleep(time.Duration(workSecs) * time.Second)
+	ch <- true
+}
+
+func timeout(timeoutChan chan bool) {
+	time.Sleep(time.Duration(timeoutSecs) * time.Second)
+	timeoutChan <- true
