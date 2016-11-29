a2ce6ebc141385eeb6d5edb1d99cb4717de9aad1
Abhijeet Mohan <void.aby@gmail.com>
2016-11-29 23:50:45 +0530

    Create rpc.go

---
+++ rpc.go
@@ -0,0 +1,40 @@
+
+// server.go
+package main
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+type Args struct {
+	X, Y int
+}
+
+type Calculator struct{}
+
+func (t *Calculator) Add(args *Args, reply *int) error {
+	*reply = args.X + args.Y
+	return nil
+}
+
+func main() {
+	cal := new(Calculator)
+	server := rpc.NewServer()
+	server.Register(cal)
+	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+	listener, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	for {
+		if conn, err := listener.Accept(); err != nil {
+			log.Fatal("accept error: " + err.Error())
+		} else {
+			log.Printf("new connection established\n")
+			go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+		}
+	}
+}
