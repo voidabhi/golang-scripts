61e207de1f140abe47039b99f8934e6ce59a67a3
Abhijeet Mohan <void.aby@gmail.com>
2017-09-18 21:12:38 +0530

    Add object pool implementation

---
+++ object_pool.go
@@ -0,0 +1,28 @@
+package pool
+
+type Pool chan *Object
+
+func New(total int) *Pool {
+	p := make(Pool, total)
+
+	for i := 0; i < total; i++ {
+		p <- new(Object)
+	}
+
+	return &p
+}
+
+
+
+// Usage:
+p := pool.New(2)
+
+select {
+case obj := <-p:
+	obj.Do( /*...*/ )
+
+	p <- obj
+default:
+	// No more objects left — retry later or fail
+	return
+}
