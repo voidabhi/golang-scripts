a3ba4a1c61e4f38e6263b57dbfcb9e97509f4e0f
Abhijeet Mohan <void.aby@gmail.com>
2017-09-21 23:56:44 +0530

    Add singleton

---
+++ singleton.go
@@ -0,0 +1,16 @@
+package singleton
+
+type singleton map[string]string
+
+var (
+    once sync.Once
+    instance singleton
+)
+
+func New() singleton {
+	once.Do(func() {
+		instance = make(singleton)
+	})
+
+	return instance
+}
