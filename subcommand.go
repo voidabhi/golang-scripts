e27e67e2004bf344cfe089ee680cdb2f1be9d659
Abhijeet Mohan <void.aby@gmail.com>
2017-12-15 23:49:10 +0530

    Add flag subcommand example

---
+++ subcommand.go
@@ -0,0 +1,54 @@
+package subcommand
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type subCommand interface {
+	Name() string
+	DefineFlags(*flag.FlagSet)
+	Run()
+}
+
+type subCommandParser struct {
+	cmd subCommand
+	fs  *flag.FlagSet
+}
+
+func Parse(commands ...subCommand) {
+	scp := make(map[string]*subCommandParser, len(commands))
+	for _, cmd := range commands {
+		name := cmd.Name()
+		scp[name] = &subCommandParser{cmd, flag.NewFlagSet(name, flag.ExitOnError)}
+		cmd.DefineFlags(scp[name].fs)
+	}
+
+	oldUsage := flag.Usage
+	flag.Usage = func() {
+		oldUsage()
+		for name, sc := range scp {
+			fmt.Fprintf(os.Stderr, "\n# %s %s\n", os.Args[0], name)
+			sc.fs.PrintDefaults()
+			fmt.Fprintf(os.Stderr, "\n")
+		}
+	}
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	cmdname := flag.Arg(0)
+	if sc, ok := scp[cmdname]; ok {
+		sc.fs.Parse(flag.Args()[1:])
+		sc.cmd.Run()
+	} else {
+		fmt.Fprintf(os.Stderr, "error: %s is not a valid command", cmdname)
+		flag.Usage()
+		os.Exit(1)
+	}
+}
